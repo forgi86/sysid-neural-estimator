8a73d27a565f98b44d72d3dec76abc7c7729ae23
marco <marco.forgione@supsi.ch>
2022-03-14 12:23:45 +0100

    - preliminary doe2 analysis - est_seq_len can also be specified as an integer value (not a fraction of seq_len) - preliminary doe3 design

--- examples/wh2009/main.py
+++ examples/wh2009/main.py
@@ -225,7 +225,7 @@ if __name__ == '__main__':
             },
                 os.path.join(model_path)
             )
-            min_loss = val_loss
+            min_loss = val_loss.item()
 
         if args.dry_run:
             break
@@ -236,7 +236,7 @@ if __name__ == '__main__':
     train_time = time.time() - start_time
     print(f"\nTrain time: {train_time:.2f}")
 
-    if not torch.isfinite(min_loss):  # model never saved as it was never giving a finite simulation loss
+    if not np.isfinite(min_loss):  # model never saved as it was never giving a finite simulation loss
         torch.save({
             "epoch": epoch,
             "args": args,
