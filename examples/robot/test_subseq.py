5596750a0b5470182d5cafd62bd4f9c7b4753e7c
forgi86 <marco.forgione1986@gmail.com>
2022-04-07 10:01:03 +0200

    working with a genious in progress

---
+++ examples/robot/test_subseq.py
@@ -0,0 +1,76 @@
+import os
+import torch
+import torchid.ss.dt.models as models
+import torchid.ss.dt.estimators as estimators
+from torchid.ss.dt.simulator import StateSpaceSimulator
+from torch.utils.data import DataLoader
+from torchid.datasets import SubsequenceDataset
+from loader import robot_loader, robot_scaling
+import matplotlib
+matplotlib.use("TKAgg")
+import matplotlib.pyplot as plt
+from torchid import metrics
+
+
+if __name__ == '__main__':
+
+    model_data = torch.load(os.path.join("models", "model.pt"))
+
+    n_dof = 6
+    n_x = 2*n_dof
+    n_u = n_dof
+    n_y = n_x
+
+    args = model_data["args"]
+
+    # Load dataset
+    t, u, y = robot_loader("test", scale=True)
+    u_mean, u_std = robot_scaling()
+
+    dataset = SubsequenceDataset(u, y, subseq_len=args.seq_len)
+    loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
+    u_t = torch.tensor(u[:, None, :])
+    y_t = torch.tensor(y[:, None, :])
+
+    #%% Load models and parameters
+    f_xu = models.MechanicalStateSpaceSystem(n_dof, hidden_size=args.hidden_size)
+    g_x = None
+    model = StateSpaceSimulator(f_xu, g_x)
+    model.load_state_dict(model_data["model"])
+
+    val_loss = 0.0
+    with torch.no_grad():
+        for batch_u, batch_y in loader:
+
+            batch_u = batch_u.transpose(0, 1)  # transpose to time_first
+            batch_y = batch_y.transpose(0, 1)  # transpose to time_first
+            batch_x0 = batch_y[0, :, :].squeeze(0)
+
+            batch_y_sim = model(batch_x0, batch_u)
+
+            # Compute fit loss
+            loss = torch.nn.functional.mse_loss(batch_y, batch_y_sim)
+            val_loss += loss
+
+    val_loss = val_loss/len(loader)
+
+    print(f"Val loss: {val_loss:.3f}")
+    #%%
+    batch_y_sim = batch_y_sim.numpy()
+    batch_y = batch_y.numpy()
+
+
+    examples = 4
+    #fig, ax = plt.subplots(examples, 1, sharex=True)
+    for batch_idx in range(examples):
+        fig, ax = plt.subplots(2, 6, sharex=True)
+        for dof_idx in range(n_dof):
+            ax[0, dof_idx].plot(batch_y[:, batch_idx, dof_idx], 'k')
+            ax[0, dof_idx].plot(batch_y_sim[:, batch_idx, dof_idx], 'b')
+            ax[0, dof_idx].plot(batch_y[:, batch_idx, dof_idx] -
+                                batch_y_sim[:, batch_idx, dof_idx], 'r')
+
+            ax[1, dof_idx].plot(batch_y[:, batch_idx, dof_idx + n_dof], 'k')
+            ax[1, dof_idx].plot(batch_y_sim[:, batch_idx, dof_idx + n_dof], 'b')
+            ax[1, dof_idx].plot(batch_y[:, batch_idx, dof_idx + n_dof] -
+                                batch_y_sim[:, batch_idx, dof_idx+n_dof], 'r')
