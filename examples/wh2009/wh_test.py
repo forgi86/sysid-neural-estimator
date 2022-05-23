330399374ddeb1f2ebc4d32b245028054a8b6202
marco <marco.forgione@supsi.ch>
2022-05-23 14:23:06 +0200

    prepared doe1 plan for pick_place

--- examples/wh2009/wh_test.py
+++ examples/wh2009/wh_test.py
@@ -19,7 +19,9 @@ if __name__ == '__main__':
     # model_data = torch.load(os.path.join("models", "doe2", "model_123.pt"), map_location=torch.device('cpu'))  # best
     # model_data = torch.load(os.path.join("models", "doe2", "model_276.pt"), map_location=torch.device('cpu'))  # worst
     # model_data = torch.load(os.path.join("models", "doe3", "model_74.pt"), map_location=torch.device('cpu'))  # best
-    model_data = torch.load(os.path.join("models", "doe4", "model_10.pt"), map_location=torch.device('cpu'))  # best
+    # model_data = torch.load(os.path.join("models", "doe4", "model_10.pt"), map_location=torch.device('cpu'))  # best
+    # model_data = torch.load(os.path.join("models", "doe5", "model_113.pt"), map_location=torch.device('cpu'))  # best
+    model_data = torch.load(os.path.join("models", "doe5", "model_420.pt"), map_location=torch.device('cpu'))  # best
 
     n_x = model_data["n_x"]
     n_y = model_data["n_y"]
